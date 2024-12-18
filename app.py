15e805f664255f97695ec35cf70b4ed821e96dd6
Matt Adewole <matt.adewole@gmail.com>
2024-12-18 10:24:33 -0600

    Configure basic Flask-Session with Redis

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import os
-from flask import Flask
+import redis
+from flask import Flask, session
+from flask_session import Session
 from schemagenerator.app.web import web as web_blueprint
 
 load_dotenv()
@@ -14,6 +16,14 @@ def create_app():
     # )
     
     app.secret_key = os.environ.get('SECRET_KEY')
+
+    # Configure Redis for storing the session data on the server-side
+    app.config['SESSION_TYPE'] = 'redis'
+    app.config['SESSION_PERMANENT'] = False
+    app.config['SESSION_USE_SIGNER'] = True
+    app.config['SESSION_REDIS'] = redis.from_url('redis://127.0.0.1:6379')
+    Session(app)
+
     # Import and register blueprints
     app.register_blueprint(web_blueprint)
     return app
