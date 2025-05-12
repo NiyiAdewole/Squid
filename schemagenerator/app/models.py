6644734c233a4f1511b9701cedc5713432820f09
Matt Adewole <matt.adewole@gmail.com>
2025-05-12 16:37:05 -0500

    Adds user model and registration form

---
+++ schemagenerator/app/models.py
@@ -0,0 +1,21 @@
+# models.py
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f'<User  {self.username}>'
+
+
+# users = {}
+
+# def add_user(username, password):
+#     if username in users:
+#         return False  # User already exists
+#     users[username] = password
+#     return True
